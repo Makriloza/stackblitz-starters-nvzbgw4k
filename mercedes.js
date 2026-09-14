6e7b690e70c801e9491d25eb540e33af52ae43c0
Makriloza <117665212+Makriloza@users.noreply.github.com>
2026-09-14 21:51:52 +0400

    Use Hoonicorn 3D model in vehicle slot

--- mercedes.js
+++ mercedes.js
@@ -1,13 +1,12 @@
-import { createCar } from './vehicles.js';
+import { loadHoonicorn } from './hoonicorn.js';
 
 export function assembleMercedes(scene) {
-  scene.name = 'Mercedes-Benz GLS 580';
+  scene.name = 'Hoonicorn Ken Block';
   return scene;
 }
 
 export async function loadMercedes() {
-  const car = createCar('suv', '#26343b');
-  car.name = 'Mercedes-Benz GLS 580';
-  car.scale.setScalar(1.05);
+  const car = await loadHoonicorn();
+  car.name = 'Hoonicorn Ken Block';
   return car;
 }
